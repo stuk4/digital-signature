cca10a08917c31256b56255de335715f3be2b0be
Bastian Perez <didier.bpp@gmail.com>
2023-02-07 19:44:32 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import { Signature } from './components/Signature'
+
+function App() {
+
+  return (
+    <div className="container">
+        <Signature 
+            width={400}
+            height={400}
+        />
+    </div>
+  )
+}
+
+export default App
